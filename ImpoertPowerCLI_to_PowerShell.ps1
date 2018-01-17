b753048e7649dcc754a47e0249531e5f4718fdf0
cernrep <35521814+cernrep@users.noreply.github.com>
2018-01-17 14:09:35 +0200

    Add files via upload

---
+++ ImpoertPowerCLI_to_PowerShell.ps1
@@ -0,0 +1,6 @@
+﻿Test-Path $profile
+New-Item -Path $profile -type file -force | Out-Null
+
+Test-Path $profile
+
+Add-Content -Path $profile -value "# Load Windows PowerShell cmdlets for managing vSphere `r`n. 'C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1'"
